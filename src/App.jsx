08651c078d4ff27cbe54318b8c17da5e31d3520f
itsparasbisht <iamparasbisht@gmail.com>
2023-08-14 20:05:28 +0530

    async action using createAsyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import "./App.css";
 import { cakeActions } from "./features/cake/cakeSlice";
+import { fetchUsers } from "./features/user/userSlice";
 import { store } from "./store";
 
 function App() {
   useEffect(() => {
-    store.dispatch(cakeActions.ordered());
+    // store.dispatch(cakeActions.ordered());
+    store.dispatch(fetchUsers());
   }, []);
 
   return <>op</>;
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "./features/cake/cakeSlice";
 import { createLogger } from "redux-logger";
 import icecreamReducer from "./features/icecream/icecreamSlice";
+import userReducer from "./features/user/userSlice";
 
 const logger = createLogger();
 
@@ -9,6 +10,7 @@ export const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
